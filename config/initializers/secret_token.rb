681f02085df7a3258a4c96341ce9befe65c212c3
amite <amit.erandole@gmail.com>
2013-07-02 21:06:59 +0530

    first commit - here we go...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catchtv::Application.config.secret_key_base = '72f20d0c1460986e793a9be844abd17cfe67007aff982e510a6ca268528e99bb7195dd4385a691d7d1a759e9be512f1ebbbd9fc16ec53fe19f886aabbb9aa990'
