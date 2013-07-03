1c80591c7221305b186d726777cf00f861787394
Lucas Mendelowski <lucas@mendelowski.com>
2013-07-03 23:29:37 +0800

    Added more gems

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,9 @@ module Catchtv
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Due to a change in Rails that prevents images from being compiled in vendor and lib,
+    # you'll need to add the following line
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
